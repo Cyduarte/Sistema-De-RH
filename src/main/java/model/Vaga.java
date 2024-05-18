a1fba6c5be5372856eed69af82ff853bfbc84b1d
Cyduart <futuristcgamez@gmail.com>
2024-05-18 13:09:38 -0300

    Criação entidade Vaga

---
+++ src/main/java/model/Vaga.java
@@ -0,0 +1,21 @@
+package model;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+@Data
+@Entity
+@Table(name = "Vaga")
+@Getter
+@Setter
+public class Vaga{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String descricao;
+    private String data;
+    private String salario;
+}
